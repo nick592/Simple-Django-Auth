2a1d5600a4a2892f40ea0d919f1e87430d6c084c
Nick E <nickevelyn7@gmail.com>
2020-05-23 00:07:09 -0400

    Redirect users to login page after registration

--- views.py
+++ views.py
@@ -13,7 +13,7 @@ def signup(request):
         form = UserCreationForm(request.POST)
         if form.is_valid():
             form.save()
-            return redirect('dashboard')
+            return redirect('login')
     else:
         form = UserCreationForm()
     return render(request, 'registration/signup.html', {'form': form})
