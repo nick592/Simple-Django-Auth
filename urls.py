cdc26a82c87d275ac193352ae4881d342a603ca0
Nick E <nickevelyn7@gmail.com>
2020-05-23 00:48:02 -0400

    Added view for when password is changed

--- urls.py
+++ urls.py
@@ -5,7 +5,8 @@ from . import views
 urlpatterns = [
     path('login/', auth_views.LoginView.as_view(), name='login'),
     path('logout/', auth_views.LogoutView.as_view(), name='logout'),
-    path('change-password/', auth_views.PasswordChangeView.as_view(), name='change_password'),
     path('dashboard/', views.dashboard, name='dashboard'),
+    path('change-password/', auth_views.PasswordChangeView.as_view(), name='change_password'),
+    path('password-change-done/', auth_views.PasswordChangeDoneView.as_view(), name='password_change_done'),
     path('signup/', views.signup, name='signup')
 ]
